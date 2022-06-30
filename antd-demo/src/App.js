b8475fba8f25d7d9a4e2ae298e18bbc218d20add
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2022-06-30 23:46:08 +0530

    setup done

---
+++ antd-demo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { FileOutlined, PieChartOutlined, UserOutlined } from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import { useState } from 'react';
+const { Header, Content, Footer, Sider } = Layout;
+//11:37
+const App = () => (
+  <div>
+  </div>
+);
+
+export default App;
+
