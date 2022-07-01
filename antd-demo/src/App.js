63ba338f245812b8386cad3a310ba2db83d4c5a4
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2022-07-01 20:52:34 +0530

    created dashboard

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -1,14 +1,206 @@
-import React from 'react';
+// import React from 'react';
+// import './App.css';
+// import { FileOutlined, PieChartOutlined, UserOutlined } from '@ant-design/icons';
+// import { Breadcrumb, Layout, Menu, Row, Col, Card, Typography } from 'antd';
+// import { useState } from 'react';
+
+// let { Title } = Typography;
+
+// const { Header, Content, Footer, Sider } = Layout;
+// const { SubMenu } = Menu;
+
+// const App = () => {
+//   const [collapsed, setCollapsed] = useState(false);
+  
+//   return (
+//     <Layout
+//       style={{
+//         minHeight: '100vh',
+//       }}
+//     >
+//       <Sider collapsible collapsed={collapsed} onCollapse={() => setCollapsed(!collapsed)}>
+//         <div className="logo" />
+//         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline"  />
+//       </Sider>
+//       <Layout className="site-layout">
+//         <Header
+//           className="site-layout-background"
+//           style={{
+//             padding: 0,
+//           }}
+//         />
+//         <Content
+//           style={{
+//             margin: '0 16px',
+//           }}
+//         >
+//           <Breadcrumb
+//             style={{
+//               margin: '16px 0',
+//             }}
+//           >
+//             <Breadcrumb.Item>User</Breadcrumb.Item>
+//             <Breadcrumb.Item>Bill</Breadcrumb.Item>
+//           </Breadcrumb>
+//           <div
+//             className="site-layout-background"
+//             style={{
+//               padding: 24,
+//               minHeight: 360,
+//             }}
+//           >
+//           <Title>Dashboard</Title>
+//             <Row>
+//               <Col>
+//                 <Card
+//                   title="Default size card"
+//                   extra={<a href="#">More</a>}
+//                   style={{
+//                     width: 300,
+//                   }}
+//                 >
+//                   <p>Card content</p>
+//                   <p>Card content</p>
+//                   <p>Card content</p>
+//                 </Card>
+//                 <Card
+//                   size="small"
+//                   title="Small size card"
+//                   extra={<a href="#">More</a>}
+//                   style={{
+//                     width: 300,
+//                   }}
+//                 >
+//                   <p>Card content</p>
+//                   <p>Card content</p>
+//                   <p>Card content</p>
+//                 </Card>
+//               </Col>
+//             </Row>
+
+//           </div>
+//         </Content>
+//         <Footer
+//           style={{
+//             textAlign: 'center',
+//           }}
+//         >
+//           Ant Design ©2018 Created by Ant UED
+//         </Footer>
+//       </Layout>
+//     </Layout>
+//   );
+// };
+
+// export default App;
 import './App.css';
-import { FileOutlined, PieChartOutlined, UserOutlined } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu } from 'antd';
+import { FileOutlined, PieChartOutlined, UserOutlined, DesktopOutlined, TeamOutlined } from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, Row, Card, Col, Typography } from 'antd';
 import { useState } from 'react';
+
+let { Title } = Typography;
+
 const { Header, Content, Footer, Sider } = Layout;
-//11:37
-const App = () => (
-  <div>
-  </div>
-);
+
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+
+const items = [
+  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('Option 2', '2', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Tom', '3'),
+    getItem('Bill', '4'),
+    getItem('Alex', '5'),
+  ]),
+  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+  getItem('Files', '9', <FileOutlined />),
+];
+
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  return (
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          className="site-layout-background"
+          style={{
+            padding: 0,
+          }}
+        />
+        <Content
+          style={{
+            margin: '0 16px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            <Title>Dashboard</Title>
+             <Row>
+               <Col>
+                 <Card
+                  title="Default size card"
+                  extra={<a href="#">More</a>}
+                  style={{
+                     width: 300,
+                   }}
+                >
+                  <p>Card content</p>
+                   <p>Card content</p>                   <p>Card content</p>
+                 </Card>
+                 <Card
+                  size="small"
+                   title="Small size card"
+                extra={<a href="#">More</a>}
+                  style={{
+                    width: 300,
+                   }}
+                 >
+                  <p>Card content</p>
+                  <p>Card content</p>
+                  <p>Card content</p>
+                 </Card>
+               </Col>
+             </Row>
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
 
 export default App;
-
