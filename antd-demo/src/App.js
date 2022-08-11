1eed5bc3a46983e6fae37d2cf7302997dfcb0f49
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2022-08-11 22:12:29 +0530

    ui

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -71,7 +71,7 @@ const App = () => {
               minHeight: 360,
             }}
           >
-            <Title>Dashboard</Title>
+            <Title>Dashboard page</Title>
              <Row>
                <Col>
                  <Card
